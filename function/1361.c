d96ad7c221ef13e33eeaab010840596108f4164a
jiyun040 <annajungarmy@gmail.com>
2024-07-09 08:52:13 +0900

    repeat
    코드업 1357, 1361

---
+++ function/1361.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i + 1; j++)
+		{
+			if (i - j <= 0)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
